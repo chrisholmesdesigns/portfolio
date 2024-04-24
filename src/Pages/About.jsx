41a0ae733fceb08aceb803aa2e5736265d14e8b6
Chris Holmes <chrisholmesdesigns@gmail.com>
2024-04-24 14:58:05 -0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -51,7 +51,7 @@ function About() {
               {arrowDown}
             </div>
           </div>
-          <img src={ChrisPicture} alt="" className="pt-[149px] z-0 w-fit h-fit" />
+          <img src={ChrisPicture} alt="" className="pt-[149px] w-fit h-fit" />
         </div>
       </div>
       <div className="bg-[#F79256] lg:text-start text-center space-y-[89px] py-[100px] padding min-h-[400px] border-t-[4px] border-t-[#425164]">
