fb74f9e9e838351b6da03b10465b574b3f6100a3
Chris Holmes <chrisholmesdesigns@gmail.com>
2024-04-24 15:41:36 -0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -20,7 +20,7 @@ function About() {
         </div>
        <div className="padding  lg:flex-row flex-col-reverse lg:space-y-0 space-y-[30px] lg:-space-x-[50px] relative flex justify-center ">
           <div>
-            <div className="bg-white mt-[30px] lg:mt-[180px] overflow-hidden border-[4px] border-[#425164] h-fit z-10 p-[24px] lg:p-[32px] lg:w-[800px] rounded-[18px] flex flex-wrap lg:flex-nowrap items-center">
+            <div className="bg-white mt-[30px] lg:mt-[180px] overflow-hidden border-[4px] border-[#425164] h-fit z-10 p-[24px] lg:p-[32px] rounded-[18px] flex flex-wrap lg:flex-nowrap items-center">
   <div>
     <p className="text-center text-[28px] lg:text-[40px] text-[#425164]">
       <span className="font-bold stroke-[5px] stroke-current">
