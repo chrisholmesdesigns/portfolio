09e1fa103972d8893de440afc302389d4bfc576d
Chris Holmes <chrisholmesdesigns@gmail.com>
2024-04-25 09:42:30 -0700

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -37,7 +37,7 @@ function Button({
         {Editable ? (
          <textarea
             placeholder={placeHolder}
-           className={`lg:text-[${textSize}px] text-[${textSm}px] flex items-center justify-center placeholder-current bg-transparent focus:outline-none text-center text-[#425164] w-full font-[800] uppercase`}
+           className={`lg:text-[${textSize}px] text-[${textSm}px] flex align-middle placeholder-current bg-transparent focus:outline-none text-center text-[#425164] w-full font-[800] uppercase`}
             onChange={(e) => onChange(e.target.value)}
           />
         ) : (
