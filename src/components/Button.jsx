9f7663067d208b0865d7fb318ea7d455d27970ef
Chris Holmes <chrisholmesdesigns@gmail.com>
2024-04-25 10:42:24 -0700

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -39,14 +39,14 @@ function Button({
          <input
            type="text"
             placeholder={placeHolder}
-           className={`lg:text-[${textSize}px] text-[${textSm}px] ml-4 mr-4 pt-4 pb-4 flex items-center justify-center placeholder-current bg-transparent focus:outline-none text-center text-[#425164] w-full font-[800] uppercase`}
+           className={`lg:text-[${textSize}px] text-[${textSm}px] ml-4 mr-4 pt-4 pb-4 flex items-center justify-center placeholder-current bg-transparent focus:outline-none text-center text-[#425164] w-auto font-[800] uppercase`}
             onChange={(e) => onChange(e.target.value)}
           />
         ) : 
          TextArea ? (
         <textarea
             placeholder={placeHolder}
-           className={`lg:text-[${textSize}px] text-[${textSm}px] ml-2 mr-2  h-72 pt-4 pb-4 flex items-center justify-center placeholder-current bg-transparent focus:outline-none text-center text-[#425164] w-full font-[800] uppercase`}
+           className={`lg:text-[${textSize}px] text-[${textSm}px] ml-2 mr-2  h-72 pt-4 pb-4 flex items-center justify-center placeholder-current bg-transparent focus:outline-none text-center text-[#425164] w-auto font-[800] uppercase`}
             onChange={(e) => onChange(e.target.value)}
           />
           )
