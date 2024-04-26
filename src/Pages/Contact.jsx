a00e76c55691ad7a1970c767f881736737f08049
Chris Holmes <chrisholmesdesigns@gmail.com>
2024-04-25 17:44:16 -0700

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -65,7 +65,7 @@ function Contact() {
               textCol="white"
             ></Button>
           </form>
-           {isSubmitted && <p className="text-green-500">Form submitted successfully!</p>}
+          {isSubmitted && <p className="text-green-500 absolute inset-x-0 bottom-0 h-16 bg-white">Form submitted successfully!</p>}
         </div>
       </div>
     </div>
