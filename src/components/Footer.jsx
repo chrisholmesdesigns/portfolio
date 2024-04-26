a8fcdd89c37fb407fe00bf3b35521b334c01cb70
Chris Holmes <chrisholmesdesigns@gmail.com>
2024-04-26 16:17:18 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import { linkedIn } from '../assets/icons'
 function Footer() {
   return (
     <footer className="h-[145px] flex items-center justify-center bg-[#A3AAB3] border-t-[4px] border-t-[#425164]">
-      {linkedIn}
+      <a href="https://www.linkedin.com/in/chrisholmesdesigns/" target="_blank" rel="noopener noreferrer">
+        {linkedIn}
+      </a>
     </footer>
   )
 }
